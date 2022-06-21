ac1615328ba7d3bf1fc8f71870e6d62ed5c55fa7
PratikshaDeshmukh0904 <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-06-21 14:55:16 +0530

    Html folder uploaded

---
+++ HTML/Html_Practice/connect.php
@@ -0,0 +1,10 @@
+<?php
+   $firstname = $_POST['firstname'];
+   $lastname = $_POST['lastname'];
+   $gender = $_POST['gender'];
+
+
+   //Database connection
+   $conn = new mysqli('localhost')
+?>
+
