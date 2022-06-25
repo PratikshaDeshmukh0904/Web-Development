a57126e30eba260fc4e9697756ef4d5ee70e87a1
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-06-25 10:15:51 +0530

    Addition

---
+++ Addition.php
@@ -0,0 +1,30 @@
+<?php
+$a=10;
+$b=20;
+
+echo "a =" .$a;
+echo "b =" .$b;
+
+$c=$a+$b;
+echo "<br>";
+echo " Addition is:$c";
+
+$d=$a-$b;
+echo "<br>";
+echo "Substraction is : $d";
+
+$e=$a*$b;
+echo "<br>";
+echo "	Multiplication is : $e";
+
+
+$f=$a/$b;
+echo "<br>";
+echo "	Division is : $f";
+
+
+
+$g=$a%$b;
+echo "<br>";
+echo "	Modulus is : $g";
+?>
