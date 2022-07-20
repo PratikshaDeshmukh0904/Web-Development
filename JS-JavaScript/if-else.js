da3b498f7caa5d9220e559d24a401067ad74a73d
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-07-20 19:54:03 +0530

    if-else

---
+++ JS-JavaScript/if-else.js
@@ -0,0 +1,21 @@
+let x="Sunday"
+
+if(x=="Monday"){
+    console.log("Monday");
+}
+else if(x=="Tuesday")
+{
+    console.log("Tuesday");
+}
+else if(x=="Wednsday")
+{
+    console.log("Wednsday");
+}
+else if(x=="Thursday")
+{
+    console.log("Thursaday");
+}
+else if(x=="Sunday");
+{
+    console.log("Sunday")
+}
