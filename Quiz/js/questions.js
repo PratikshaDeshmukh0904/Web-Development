bd7ca71b7ca0e0ddde6827cca0eba3a3e1855adf
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-11-24 16:24:28 +0530

    Online Quiz Using JavaScript

---
+++ Quiz/js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " Which type of JavaScript language is ___",
+    answer: "Object-Based",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Assembly-language",
+      "High-level"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which one of the following also known as Conditional Expression",
+    answer: "immediate if",
+    options: [
+      "Alternative to if-else",
+      "Switch statement",
+      "If-then-else statement",
+      "immediate if"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The function and var are known as:",
+    answer: "Declaration statements",
+    options: [
+      "Keywords",
+      "Data types",
+      "Declaration statements",
+      "Prototypes"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not an error in JavaScript?",
+    answer: "Division by zero",
+    options: [
+      "Missing of Bracket",
+      "Division by zero",
+      "Syntax error",
+      "Missing of semicolons"
+    ]
+  },
+    {
+    numb: 5,
+    question: "CSS stands for",
+    answer: "Cascading style sheets",
+    options: [
+      "Cascade style sheets",
+      "Color and style sheets",
+      "Cascading style sheets",
+      "None of the above",
+    ]
+  },
+  {
+    numb: 6,
+    question: "The property in CSS used to change the background color of an element is",
+    answer: "background-color",
+    options: [
+      "bgcolor",
+      "color",
+      "background-color",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which of the following has introduced text, list, box, margin, border, color, and background properties?",
+    answer: "CSS",
+    options: [
+      "HTML",
+      "PHP",
+       "CSS",
+       "Ajax"
+    ]
+  },
+    {
+    numb: 8,
+    question: "The CSS property used to specify the transparency of an element is",
+    answer: "opacity",
+    options: [
+      "opacity",
+      "filter",
+      "visibility",
+      "overlay"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Which of the following is not a type of combinator?",
+    answer: "*",
+    options: [
+      ">",
+      "~",
+      "+",
+      "*"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Which of the following scoping type does JavaScript use",
+    answer: "font-size",
+    options: [
+      "font-size",
+      "text-size",
+       "text",
+       "size"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
