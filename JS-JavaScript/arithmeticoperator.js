61305a44320e0d9a1cea757f28091350fdb142a3
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-01-14 22:37:17 +0530

    JavaScript_Course

---
+++ JS-JavaScript/arithmeticoperator.js
@@ -0,0 +1,42 @@
+//Arithmetic Operator
+//1.Addition +
+//2.Substraction -
+//3.Multiplication *
+//4.Divide /
+//5.Reminder %
+//6.Increment ++
+//7.Decrement --
+//8.Exponention **
+
+
+var a = 10;
+var b = 5;
+
+var add = a+b;
+var sub = a-b;
+var mul = a*b;
+var div = a/b;
+
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
+
+
+var red = a%b;
+console.log(red);
+
+var num0=11;
+num0++; //Increment by 1
+console.log(num0);
+
+num0--;
+console.log(num0);
+
+var num3=3;
+var ex = num3 ** 2;  //Power of number
+console.log(ex);
+
+
+var ex = num3 ** 5;  //Power of number
+console.log(ex);
